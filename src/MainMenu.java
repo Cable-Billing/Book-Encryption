e5a882e77a151a7f8d2c1838723cbab2c2618e7b
DoneRaging <Billing.caleb14@gmail.com>
2019-05-16 17:14:04 +0930

    First Commit

---
+++ src/MainMenu.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenu {
+    private JButton encryptButton;
+    private JButton decryptButton;
+
+    public MainMenu() {
+        encryptButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        decryptButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
